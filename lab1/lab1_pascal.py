3600718f8de769d2e5cc7d2c4c3a262c02d226e2
liyzcj <liyz0912@gmail.com>
2018-10-08 01:24:09 +0300

    format code

--- lab1/lab1_pascal.py
+++ lab1/lab1_pascal.py
@@ -1,31 +1,30 @@
 from random import randint
 from time import sleep
+
+
 class Agent:
 
-    rownumber = 0
+    row_number = 0
     name = 'name'
 
-    def __init__(self,rownumber):
+    def __init__(self, row_number):
 
-        self.rownumber = rownumber
-        self.name = 'Agent' + str(rownumber)
-
-
+        self.row_number = row_number
+        self.name = 'Agent' + str(row_number)
 
     def action(self, last_line):
-        print('Name:{},  Location:Line{}, '.format(self.name,self.rownumber), end=" ")
+        print('Name:{},  Location:Line{}, '.format(self.name, self.row_number), end=" ")
 
-        if self.rownumber == 1:
+        if self.row_number == 1:
             new_line = [1]
             print("=[1]")
-        elif last_line == []:
-            new_line = [] 
+        elif not last_line:
+            new_line = []
             print('Wait...')
         else:
             new_line = self.compute(last_line[:])
             print("compute {}".format(new_line))
         return new_line
-
 
     @staticmethod
     def compute(last_line):
@@ -36,22 +35,26 @@ class Agent:
             new_line[i] = last_line[i-1]+last_line[i]
         
         return new_line
+
+
 '''
 function : generate the correct result of pascal triangle
 input  : n -- the number of line
 output : A list contain each line of pascal triangle
 '''
+
+
 def gen_correct_res(row_number):
+
     pascal = [[1]]
     for i in range(row_number-1):
         pascal.append(Agent.compute(pascal[i][:]))
     return pascal
 
 
-
 def sos(row_number):
-    pascal = [[] for i in range(row_number)]
-    agents = [Agent(i) for i in range(1,row_number+1)]
+    pascal = [[]] * row_number
+    agents = [Agent(i) for i in range(1, row_number+1)]
 
     # print (agents)
     "generate the correct pascal triangle first"
@@ -59,8 +62,8 @@ def sos(row_number):
     # print(correct_res)
     # print(pascal)
     while True:
-        "generate random number to simulate multithreading"
-        i = randint(1,row_number) - 1 
+        "generate random number to simulate multi threading"
+        i = randint(0, row_number - 1)
         last_line = pascal[i-1]
         "agent action"
         pascal[i] = agents[i].action(last_line)
@@ -72,14 +75,9 @@ def sos(row_number):
     print('ok')
     
 
-
-
-
-
 if __name__ == '__main__':
     "generate correct pascal triangle"
     # pascal = gen_correct_res(10)
     # for line in pascal:
-        # print(line)
-    
+    # print(line)
     sos(6)
--- lab1/lab1_prime.py
+++ lab1/lab1_prime.py
@@ -11,50 +11,45 @@ class Agent:
 
     def delete_one(self, allnumber):
         delete_num = None
-        for n in allnumber:
-            if n % self.number == 0 and n != self.number:
-                allnumber.remove(n)
-                delete_num = n
+        for num in allnumber:
+            if num % self.number == 0 and num != self.number:
+                allnumber.remove(num)
+                delete_num = num
                 break
         print("Name: {},  Action: Remove {}".format(self.name, delete_num))
 
-        
-                
-
-    
 
 def gen_correct_res(number):
     num = []
-    for i in range(2,number):
-        j = 2 
+    for i in range(2, number):
         for j in range(2, i):
-            if i%j == 0 :
+            if i % j == 0:
                 break
         else:
             num.append(i)
     return num
 
 
-def sos(n):
-    numbers = [i + 1 for i in range(n)]
+def sos(g):
+    numbers = [i + 1 for i in range(g)]
     numbers.remove(1)
     # print(numbers)
     prime = gen_correct_res(n)
-    print("trueprime ={} ".format(prime))
+    print("true prime ={} ".format(prime))
     # print(prime)
     agents = [Agent(i) for i in numbers]
     # print(agents)
     while True:
         print("Current Numbers: {} ".format(numbers))
-        index = randint(0,len(prime)-1)
+        index = randint(0, len(prime)-1)
         agents[index].delete_one(numbers)
         sleep(1)
         if numbers == prime:
             print("numbers ==== {}".format(numbers))
             print("OK")
             break
+
+
 if __name__ == '__main__':
     n = 30
     sos(n)
-
-    
