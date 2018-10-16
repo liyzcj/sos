7e451a6cd5e73ede63482b56593e82e012c55042
liyzcj <liyz0912@gmail.com>
2018-10-16 08:55:56 +0300

    add temporal files

---
+++ lab2/tmp/Dp.py
@@ -0,0 +1,11 @@
+"""
+Created on Mon Oct 15 04:03:16 2018
+
+@author: li yanzhe
+"""
+
+class Dp:
+    def __init__(self, m, start):
+        self.m = m  # map
+        self.start = start  # start node
+        self.array
